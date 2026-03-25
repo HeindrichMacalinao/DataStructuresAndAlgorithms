51ff1df636c653928fa375c29525eb8038685062
Heindrich Macalinao <heindrich.macalinao@gmail.com>
2026-03-26 00:43:03 +0800

    Clean up comments in LinkedList.java
    Removed comments for linked list creation and element addition.

--- Data Structures/LinkedList.java
+++ Data Structures/LinkedList.java
@@ -4,10 +4,8 @@ public class Sandbox {
 
     public static void main(String[] args) {
         
-        // create linkedlist
         LinkedList<String> animals = new LinkedList<>();
-
-        // Add elements to LinkedList
+        
         animals.add("Dog");
         animals.add("Cat");
         animals.add("Cow");
