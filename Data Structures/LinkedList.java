bccb46a8a99d41116f8ced19839ce299cf28130b
Heindrich Macalinao <heindrich.macalinao@gmail.com>
2026-03-25 23:42:44 +0800

    Add LinkedList example in LinkedList.java
    This file contains a simple example of using a LinkedList in Java to store and print a list of animals.

---
+++ Data Structures/LinkedList.java
@@ -0,0 +1,19 @@
+package prac;
+
+import java.util.LinkedList;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        
+        // create linkedlist
+        LinkedList<String> animals = new LinkedList<>();
+
+        // Add elements to LinkedList
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Cow");
+        
+        System.out.println("LinkedList: " + animals);
+    }
+}
