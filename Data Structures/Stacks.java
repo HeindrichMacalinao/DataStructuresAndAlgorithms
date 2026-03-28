eed29cb2ecaeff47c2c32852463ed4d80360a12f
Heindrich Macalinao <heindrich.macalinao@gmail.com>
2026-03-27 18:53:51 -1000

    Refactor stack example by removing comments
    Removed comments and adjusted formatting in the stack example.

--- Data Structures/Stacks.java
+++ Data Structures/Stacks.java
@@ -6,20 +6,17 @@ public class Sandbox
 {
     public static void main(String[] args) 
     {
-        // Creating a stack of Strings
         Stack<String> myStack = new Stack<>();
 
         System.out.println("Is my stack empty? " + myStack.empty());
 
-        // Pushing elements onto the stack
         myStack.push("Orange Ball");
         myStack.push("Violet Ball");
         myStack.push("Green Ball");
 
         System.out.println("Elements in Stack: " + myStack);
         System.out.println("Is my stack empty? " + myStack.empty());
-
-        // Popping elements until the stack is empty
+        
         while (!myStack.isEmpty()) {
             myStack.pop();
             System.out.println("Elements in Stack: " + myStack);
